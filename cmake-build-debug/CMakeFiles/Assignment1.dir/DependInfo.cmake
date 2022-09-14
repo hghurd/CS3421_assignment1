1d840f411def510543d8afb7d2e44fa03ac58952
hghurd <hghurd@mtu.edu>
2022-09-14 07:31:18 -0400

    Implemented cpu.reset()

---
+++ cmake-build-debug/CMakeFiles/Assignment1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hghurd/CLionProjects/CS3421_assignment1/clock.c" "CMakeFiles/Assignment1.dir/clock.c.o" "gcc" "CMakeFiles/Assignment1.dir/clock.c.o.d"
+  "/home/hghurd/CLionProjects/CS3421_assignment1/command_parser.c" "CMakeFiles/Assignment1.dir/command_parser.c.o" "gcc" "CMakeFiles/Assignment1.dir/command_parser.c.o.d"
+  "/home/hghurd/CLionProjects/CS3421_assignment1/cpu.c" "CMakeFiles/Assignment1.dir/cpu.c.o" "gcc" "CMakeFiles/Assignment1.dir/cpu.c.o.d"
+  "/home/hghurd/CLionProjects/CS3421_assignment1/main.c" "CMakeFiles/Assignment1.dir/main.c.o" "gcc" "CMakeFiles/Assignment1.dir/main.c.o.d"
+  "/home/hghurd/CLionProjects/CS3421_assignment1/memory.c" "CMakeFiles/Assignment1.dir/memory.c.o" "gcc" "CMakeFiles/Assignment1.dir/memory.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "Assignment1"
+  "Assignment1.pdb"
+  "CMakeFiles/Assignment1.dir/clock.c.o"
+  "CMakeFiles/Assignment1.dir/clock.c.o.d"
+  "CMakeFiles/Assignment1.dir/command_parser.c.o"
+  "CMakeFiles/Assignment1.dir/command_parser.c.o.d"
+  "CMakeFiles/Assignment1.dir/cpu.c.o"
+  "CMakeFiles/Assignment1.dir/cpu.c.o.d"
+  "CMakeFiles/Assignment1.dir/main.c.o"
+  "CMakeFiles/Assignment1.dir/main.c.o.d"
+  "CMakeFiles/Assignment1.dir/memory.c.o"
+  "CMakeFiles/Assignment1.dir/memory.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
